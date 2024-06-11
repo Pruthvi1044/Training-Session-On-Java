2165b208d8209a61c31f1c93f9122d39a76574a0
Pruthvi1044 <pruthvirajkhedkar23@gmail.com>
2024-06-11 14:59:10 +0530

    This file is relates to function

---
+++ TestFun2.java
@@ -0,0 +1,16 @@
+package pk.com;
+
+public class TestFun2 {
+
+	public static void main(String[] args) {
+		
+		//System.out.println("Hello world");
+		
+		//TestFunction f = new TestFunction();
+		//f.add();
+		
+		TestFunction.add();
+		TestFunction.sub(12,15);
+	}
+
+}
---
+++ TestFunction.java
@@ -0,0 +1,19 @@
+package pk.com;
+
+public class TestFunction {
+	public static void add() {
+		int a=10;
+		int b=20;
+		int Total=a+b;
+		System.out.println("Total is : "+Total);
+	}
+	
+	public static void sub(int a,int b) {
+		int d=10;
+		int e=20;
+		int Total=a-b;
+		System.out.println("Total is : "+Total);
+	}
+	
+	
+}
